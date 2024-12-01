fb4fa1862ce66fd1b202bfb5e5c308beff28d6f1
Nuzul <farhan.nuzul@ui.ac.id>
2024-12-01 15:35:34 +0700

    progress

---
+++ app/src/main/java/com/kelompok12/uistride/Login.kt
@@ -0,0 +1,11 @@
+package com.kelompok12.uistride
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
